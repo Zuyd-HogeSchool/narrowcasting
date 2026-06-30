7a745fd006b9eb681e7ab395c7d89e970b3f68e3
Bradley <b.malasch@hotmail.com>
2026-06-30 14:58:59 +0200

    Models Department, Screen, Playlist, PlaylistItem en MediaFile aangemaakt.

---
+++ Narrowcasting/Models/Playlist.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Narrowcasting.Models
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //FK
+        [Range(1, int.MaxValue, ErrorMessage = "Selecteer een scherm")]
+        public int ScreenId { get; set; }
+        public string CreatedById { get; set; } = string.Empty;
+
+        //Navigation
+        public Screen? Screen { get; set; }
+        public ICollection<PlaylistItem> PlaylistItems { get; set; } = new List<PlaylistItem>();
+    }
+}
